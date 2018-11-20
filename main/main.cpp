771b1728492725a69ae6adff552ed138c6be9579
BuildTools <unconfigured@null.spigotmc.org>
2018-11-20 09:40:08 -0500

    d

--- main/main.cpp
+++ main/main.cpp
@@ -7,9 +7,17 @@
 using namespace std;
 
 int main() {
-    
+    timer();
 }
 
 int timer(){
-    
+     boost::asio::io_service io;
+ 
+  for(int i = 0; i < 5; i++) {
+    boost::asio::deadline_timer timer(io, boost::posix_time::seconds(i));
+    timer.wait();
+    std::cout << "Blocking wait(): " << i << " second-wait\n";
+  }
+
+  return 0;
 }
