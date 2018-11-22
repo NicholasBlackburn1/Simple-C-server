26b5d99d26acd5f664274a38f849b5ee9244f7e6
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 12:29:11 -0500

    ww

--- main/client.rb
+++ main/client.rb
@@ -3,9 +3,10 @@ require 'rubygems'
 require 'excon'
     puts "#{RuTui::Ansi.fg(49)} __RUBY HTTP CLIENT__"
     print RuTui::Ansi.clear_color
-    puts"#{RuTui::Ansi.fg(11)} starting http responce"
+    puts"#{RuTui::Ansi.fg(90)} starting http responce"
 response = Excon.get('http://192.168.1.1:8080')
 puts response.remote_ip 
 puts response.status  
-puts response.get
+puts "#{RuTui::Ansi.fg(196)} HTTP HEADER"
+puts response.body
 print RuTui::Ansi.clear_color
