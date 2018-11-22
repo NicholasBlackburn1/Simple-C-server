3b046e662a803e9871257862219eaf839b90f15b
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 11:10:44 -0500

    ruby http client

--- main/client.rb
+++ main/client.rb
@@ -1,2 +1,4 @@
-require 'faraday'
-Faraday.new('http://localhost:8080', headers: { 'User-Agent' => 'test' }).get
+require 'rutui'
+screen = RuTui::Screen.new
+screen.add_static RuTui::Text.new({ :x => 50, :y  => 1, :text => "Ruby HTTP client" })
+screen.draw
