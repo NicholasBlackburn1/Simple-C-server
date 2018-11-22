b0850659957c6dbeac2978f55d9716fb61dcfaa6
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 12:36:13 -0500

    sss

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ![alt text](https://img.icons8.com/cotton/2x/server.png)
-# Simple-C-server
+# Simple-C++-http-server
+## With an ruby http responce client 
+1. simple to use 
+2. crossplatform
+3. light-weight
--- main/client.rb
+++ main/client.rb
@@ -3,10 +3,12 @@ require 'rubygems'
 require 'excon'
     puts "#{RuTui::Ansi.fg(49)} __RUBY HTTP CLIENT__"
     print RuTui::Ansi.clear_color
-    puts"#{RuTui::Ansi.fg(90)} starting http responce"
+    puts"#{RuTui::Ansi.fg(11)} starting http responce"
 response = Excon.get('http://192.168.1.1:8080')
 puts response.remote_ip 
 puts response.status  
-puts "#{RuTui::Ansi.fg(196)} HTTP HEADER"
+puts "#{RuTui::Ansi.fg(49)} --HTTP HEADER--"
+print RuTui::Ansi.clear_color
+puts"#{RuTui::Ansi.fg(11)} "
 puts response.body
 print RuTui::Ansi.clear_color
