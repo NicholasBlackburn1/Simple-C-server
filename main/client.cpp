dba5237a8f8c2d59f7af9a1a752488e600a94deb
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 10:50:16 -0500

    added ruby http client

--- main/client.cpp
+++ main/client.cpp
@@ -1,3 +1,8 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
 #include <iostream>
 #include <unistd.h> 
 #include <stdio.h> 
@@ -5,7 +10,6 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-#define PORT 8080 
    
 int main(int argc, char const *argv[]) 
 { 
@@ -26,10 +30,10 @@ int main(int argc, char const *argv[])
     serv_addr.sin_port = htons(PORT); 
        
     // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(AF_INET, "127.0.0.1", &serv_addr.sin_addr == 0)
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
     { 
         printf("\nInvalid address/ Address not supported \n"); 
-        return 0; 
+        return -1; 
     } 
    
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
@@ -42,4 +46,3 @@ int main(int argc, char const *argv[])
     valread = read( sock , buffer, 1024); 
     printf("%s\n",buffer ); 
     return 0; 
-} 
