e1565d986e8a478b249a4455e502329efd6dcb65
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 12:03:23 -0500

    added ruby client

--- main/client.rb
+++ main/client.rb
@@ -1,9 +1,10 @@
 require 'rutui'
-screen = RuTui::Screen.new
-screen.add_static RuTui::Text.new({ :x => 50, :y  => 1, :text => "Ruby HTTP client" })
-screen.add_static RuTui::Text.new({ :x => 50, :y  => 11, :text => "Hit q to exit" })
+require 'rubygems'
+require 'excon'
+    print "#{RuTui::Ansi.fg(49)} __RUBY HTTP CLIENT__"
+    print RuTui::Ansi.clear_color
+    print "#{RuTui::Ansi.fg(11)} starts 
+response = Excon.get('http://geemus.com')
+print response.body
 
-RuTui::ScreenManager.loop({ :autofit => true, :autodraw => false }) do |key|
-	break if key.chr == "q" # 3 = CTRL+C
-    end
-screen.draw
+print RuTui::Ansi.clear_color
