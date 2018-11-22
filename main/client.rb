7efd0500a3e7ccc994d047600f47ec621f215a6b
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 12:05:50 -0500

    added

--- main/client.rb
+++ main/client.rb
@@ -3,8 +3,10 @@ require 'rubygems'
 require 'excon'
     print "#{RuTui::Ansi.fg(49)} __RUBY HTTP CLIENT__"
     print RuTui::Ansi.clear_color
-    print "#{RuTui::Ansi.fg(11)} starts 
+    print "#{RuTui::Ansi.fg(11)} starting http responce"
 response = Excon.get('http://geemus.com')
 print response.body
-
+print response.headers 
+print response.remote_ip 
+print response.status  
 print RuTui::Ansi.clear_color
