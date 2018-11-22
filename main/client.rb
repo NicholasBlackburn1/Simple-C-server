65cb2a6a17f4cb967edea12d8ab0be8de37dbf7f
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 11:15:34 -0500

    added tui

--- main/client.rb
+++ main/client.rb
@@ -1,4 +1,9 @@
 require 'rutui'
 screen = RuTui::Screen.new
 screen.add_static RuTui::Text.new({ :x => 50, :y  => 1, :text => "Ruby HTTP client" })
+screen.add_static RuTui::Text.new({ :x => 50, :y  => -5, :text => "Hit q to exit" })
+
+RuTui::ScreenManager.loop({ :autofit => true, :autodraw => false }) do |key|
+	break if key.chr == "q" # 3 = CTRL+C
+    end
 screen.draw
