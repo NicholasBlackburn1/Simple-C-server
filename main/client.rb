331e39079fac9c6607d4213cbd09d134cb1b4800
BuildTools <unconfigured@null.spigotmc.org>
2018-11-27 10:57:56 -0500

    ddd

--- main/client.rb
+++ main/client.rb
@@ -8,9 +8,12 @@ require 'excon'
     puts"#{RuTui::Ansi.fg(11)} starting http responce"
     
 response = Excon.get('http://localhost:80')
-puts "#{RuTui::Ansi.fg(49)} --STATUS"
+puts "#{RuTui::Ansi.fg(49)} --STATUS--"
+print RuTui::Ansi.clear_color
+puts"#{RuTui::Ansi.fg(11)} "
 puts response.remote_ip 
 puts response.status  
+print RuTui::Ansi.clear_color
 puts "#{RuTui::Ansi.fg(49)} --HTTP HEADER--"
 print RuTui::Ansi.clear_color
 puts"#{RuTui::Ansi.fg(11)} "
